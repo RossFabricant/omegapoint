64b700ded48d6a3a7b01aad151b44383faa96669
RossFabricant <58613925+RossFabricant@users.noreply.github.com>
2020-06-23 20:36:42 -0400

    Move to correct directory structure.

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="omegapoint",
     version="0.0.1",
+    scripts=[],
     author="Ross Fabricant",
     description="Omega Point API Python Library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/RossFabricant/omegapoint",
-    packages=setuptools.find_packages(),
+    packages=['omegapoint'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
