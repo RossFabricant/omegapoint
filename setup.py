6d4386798502ae9628b92ac7e00b18094ba64b50
Ross Fabricant <58613925+RossFabricant@users.noreply.github.com>
2021-04-20 13:03:47 -0400

    Bug fix

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/RossFabricant/omegapoint",
     packages=['omegapoint'],
-    install_requires=['numpy','pandas','request','sgqlc'],
+    install_requires=['numpy','pandas','requests','sgqlc'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
